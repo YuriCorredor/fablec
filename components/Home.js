4a863bc4e4346e1cb51bbc629563ade1f35d6e35
Yuri Corredor <YuriCorredor159@gmail.com>
2022-03-18 16:35:40 -0300

    home page without animations

--- components/Home.js
+++ components/Home.js
@@ -5,10 +5,14 @@ export default function Home() {
     }
 
     return (
-        <div className="bg-black h-screen">
-            <h1 className="">A parceira de mais de 50 Escolas brasileiras</h1>
-            <p className="">Trabalhamos com todos os equipamentos necessários para a sua escola ou creche.</p>
-            <button className="z-10 text-[#20252a] p-4 px-8 sm:px-20 m-2 mt-8 bg-[#42ec9a] hover:bg-[#0fcc7d] font-bold rounded-full text-base sm:text-xl">Contate-nos</button>
+        <div className="pt-80 sm:pt-32 h-screen flex flex-col justify-center items-center sm:items-start sm:flex-row">
+            <div className="flex max-w-lg flex-col items-center">
+                <p className="pb-8 text-[#0fcc7d] font-thin text-xs">ESCOLAS E CRECHES</p>
+                <h1 className="text-[#2d3748] text-4xl font-bold p-4 pt-0 text-center sm:text-left">A parceira de mais de 50 Escolas e Creches brasileiras</h1>
+                <p className="text-[#718096] text-lg font-medium p-4 pb-0 pt-0 text-center sm:text-left">Trabalhamos com todos os equipamentos necessários para a sua Escola ou Creche. Entre em contato para fazer parte de nossa equipe.</p>
+                <button className="text-[#2d3748] p-4 px-12 sm:px-20 m-2 sm:mt-16 md:mt-28 bg-[#42ec9a] hover:bg-[#0fcc7d] font-bold rounded-full text-base sm:text-xl">Contate-nos</button>
+            </div>
+            <img src='einstein.png' />
         </div>
     )
 }
