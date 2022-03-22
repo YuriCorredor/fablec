904bef4eb626750fd53fc4f9cb23798ca3c15210
Yuri Corredor <YuriCorredor159@gmail.com>
2022-03-22 20:01:52 -0300

    Added title to index and animation to the Einstein png when he comes into view

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,58 @@
-import { motion } from 'framer-motion'
+import { useEffect } from 'react'
+import { motion, useAnimation } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+
+const outerDivVariants = {
+    hidden: {
+        x: -1000, 
+        opacity: 0
+    },
+    visible: {
+        x: 0, 
+        opacity: 1,
+        transition: {
+            duration: 0.5,
+            //when: "beforeChildren",
+            //staggerChildren: 0.5 (seconds)
+        }
+    }
+}
+
+const imageVariants = {
+    hidden: {
+        scale: 0,
+        opacity: 0
+    },
+    visible: {
+        scale: 1,
+        opacity: 1,
+        transition: {
+            duration: 0.8
+        }
+    }
+}
 
 export default function Home() {
+
+    const controls = useAnimation()
+    const { ref, inView } = useInView()
+
+    useEffect(() => {
+        if (inView) {
+            controls.start('visible')
+        }
+        if (!inView) {
+            controls.start('hidden')
+        }
+    }, [controls, inView])
+
     return (
         <motion.div
             id="home"
             className="pt-28 pb-12 sm:pb-40 flex flex-col justify-center items-center md:items-start md:flex-row"
-            initial={{ x: -1000, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ duration: 0.5 }}
+            variants={outerDivVariants}
+            initial="hidden"
+            animate="visible"
         >
             <div className="flex max-w-lg flex-col items-center">
                 <p className="pb-8 text-[#0fcc7d] font-thin text-xs">ESCOLAS E CRECHES</p>
@@ -15,11 +60,11 @@ export default function Home() {
                 <p className="text-[#718096] text-lg font-medium p-4 pb-0 pt-0 text-center sm:text-left">Trabalhamos com todos os equipamentos necessários para a sua Escola ou Creche. Entre em contato e faça o seu orçamento, e receberá ofertas incríveis.</p>
                 <a href="#contact" className="text-[#2d3748] p-4 px-12 sm:px-18 m-2 mt-12 md:mt-24 bg-[#42ec9a] hover:bg-[#0fcc7d] font-bold rounded-full text-base sm:text-xl">Contate-nos</a>
             </div>
-            <motion.img 
+            <motion.img
+                ref={ref}
                 className="w-auto h-auto" src='einstein.png'
-                initial={{ scale: 0, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
-                transition={{ delay: 0.6, duration: 0.5 }}
+                variants={imageVariants}
+                animate={controls}
             />
         </motion.div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import NavBar from "../components/NavBar"
 import Home from "../components/Home"
 import Services from "../components/Services"
+import Head from "next/head"
 
 
 export default function index() {
     return (
         <>
+            <Head>
+                <title>FABLEC DISTRIBUIDORA</title>
+            </Head>
             <NavBar />
             <Home />
             <Services />
