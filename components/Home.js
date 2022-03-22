1e9cd0283da4ce43225ff09f2b2b92a7f8a4c370
Yuri Corredor <YuriCorredor159@gmail.com>
2022-03-22 13:37:05 -0300

    Changed text and add href to button so it goes to contact

--- components/Home.js
+++ components/Home.js
@@ -12,8 +12,8 @@ export default function Home() {
             <div className="flex max-w-lg flex-col items-center">
                 <p className="pb-8 text-[#0fcc7d] font-thin text-xs">ESCOLAS E CRECHES</p>
                 <h1 className="text-[#2d3748] text-5xl font-semibold p-4 pt-0 text-center sm:text-left">A parceira de mais de 50 Escolas e Creches brasileiras</h1>
-                <p className="text-[#718096] text-lg font-medium p-4 pb-0 pt-0 text-center sm:text-left">Trabalhamos com todos os equipamentos necessários para a sua Escola ou Creche. Entre em contato para fazer parte de nossa equipe.</p>
-                <button className="text-[#2d3748] p-4 px-12 sm:px-18 m-2 mt-12 md:mt-24 bg-[#42ec9a] hover:bg-[#0fcc7d] font-bold rounded-full text-base sm:text-xl">Contate-nos</button>
+                <p className="text-[#718096] text-lg font-medium p-4 pb-0 pt-0 text-center sm:text-left">Trabalhamos com todos os equipamentos necessários para a sua Escola ou Creche. Entre em contato e faça o seu orçamento, e receberá ofertas incríveis.</p>
+                <a href="#contact" className="text-[#2d3748] p-4 px-12 sm:px-18 m-2 mt-12 md:mt-24 bg-[#42ec9a] hover:bg-[#0fcc7d] font-bold rounded-full text-base sm:text-xl">Contate-nos</a>
             </div>
             <motion.img 
                 className="w-auto h-auto" src='einstein.png'
