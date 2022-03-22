9b0eba1f664b31492fd5724ea2ff3cb833487576
Yuri Corredor <YuriCorredor159@gmail.com>
2022-03-22 13:38:05 -0300

    better

--- components/ServiceItem.js
+++ components/ServiceItem.js
@@ -1,9 +1,9 @@
-export default function ServiceItem({ imgSrc, title, description }) {
+export default function ServiceItem({ Icon, title, description }) {
     return (
-        <div className="flex flex-col items-center place-self-center center w-full break-all">
-            <img width={120} src={imgSrc} />
-            <h1 className="text-[#2d3748] text-xl font-semibold p-4 pt-0 break-normal text-center">{title}</h1>
-            <p className="">dasd;lksam;klmsadkisjadsjaasddikjsakipdasaisodjnaskdioasjdasdiasjdas</p>
+        <div className="flex flex-col items-center max-w-[300px] p-2 hover:scale-110 transition-all cursor-pointer">
+            <Icon size={38} color='#0fcc7d' className='m-2' />
+            <h1 className="text-[#2d3748] text-xl font-semibold max-w-[240px] p-4 py-0 pb-1 text-center">{title}</h1>
+            <p className="text-[#718096] text-md p-0 text-center">{description}</p>
         </div>
     )
 }
--- components/Services.js
+++ components/Services.js
@@ -1,44 +1,50 @@
 import ServiceItem from "./ServiceItem"
+import { FiPackage } from "react-icons/fi"
+import { BiConversation } from "react-icons/bi"
+import { MdOutlineSchool } from "react-icons/md"
+import { IoConstructOutline, IoPersonOutline } from "react-icons/io5"
+import { BsPinMap } from "react-icons/bs"
+
 
 const items = [
     {
-        imgSrc: 'https://dropify.com.br/assets/img/logos/logo-dropify-blue.png',
+        Icon: FiPackage,
         title: 'Sem pedido mínimo',
-        description: 'Test1Test1Test1Test1'
+        description: 'Compre produtos no atacado sem quantidade ou pedido mínimo.'
     },
     {
-        imgSrc: 'https://dropify.com.br/assets/img/logos/logo-dropify-blue.png',
+        Icon: BiConversation,
         title: 'Transparência',
-        description: 'Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1'
+        description: 'Um bom negócio não precisa ir na direção oposta ao que você acredita. Aqui possuímos responsabilidade com os seus valores.'
     },
     {
-        imgSrc: 'https://dropify.com.br/assets/img/logos/logo-dropify-blue.png',
+        Icon: MdOutlineSchool,
         title: 'Materiais Educacionais',
-        description: 'Test1Test1'
+        description: 'Material de escritório, informática, impressoras e cartuchos, material escolar e muito mais. Os melhores produtos para a sua escola/creche com ofertas imperdíveis estão aqui, na Fablec.'
     },
     {
-        imgSrc: 'https://dropify.com.br/assets/img/logos/logo-dropify-blue.png',
-        title: 'Construção e Reforma',
-        description: 'Test1Test1'
+        Icon: IoConstructOutline,
+        title: 'Construção, Reforma e Instalação',
+        description: 'Nós executamos reformas desde pequenas intervenções a grandes reformas. Adequação de ambientes para as normas de acessibilidade das escolas.'
     },
     {
-        imgSrc: 'https://dropify.com.br/assets/img/logos/logo-dropify-blue.png',
+        Icon: BsPinMap,
         title: 'Tudo em um único lugar',
-        description: 'Test1Test1'
+        description: 'Oferecemos tudo o que precisa em um único lugar, sem complicação e com toda comodidade e conveniência para você.'
     },
     {
-        imgSrc: 'https://dropify.com.br/assets/img/logos/logo-dropify-blue.png',
+        Icon: IoPersonOutline,
         title: 'Foco nas necessidades dos clientes',
-        description: 'Test1Test1'
+        description: 'Nos adaptamos para cumprir com quaisquer necessidades que você tiver.'
     }
 ]
 
 export default function Services() {
     return (
-        <div id='services' className="flex flex-col items-center justify-center overflow-hidden">
-            <h1 className="text-[#2d3748] text-3xl font-semibold text-center p-4 pt-0">Vantagens de nossos serviços</h1>
-            <div className="grid grid-cols-2 sm:grid-cols-3 p-8 gap-8 w-screen">
-                {items.map(i => <ServiceItem imgSrc={i.imgSrc} title={i.title} description={i.description} />)}
+        <div id='services' className="flex flex-col mx-2 items-center justify-center overflow-hidden">
+            <h1 className="text-[#2d3748] text-3xl font-semibold text-center p-4 sm:pb-16 pt-0">Vantagens de nossos serviços</h1>
+            <div className="grid sm:grid-cols-2 lg:grid-cols-3 p-8 gap-8 w-screen justify-items-center">
+                {items.map((i, index) => <ServiceItem key={index} Icon={i.Icon} title={i.title} description={i.description} />)}
             </div>
         </div>
     )
