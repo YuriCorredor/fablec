c3232e2f0a8191c00fcef652635093877614fd1a
Yuri Corredor <YuriCorredor159@gmail.com>
2022-03-23 15:19:11 -0300

    Create button compenent to reuse it in AboutUs.js

---
+++ components/Button.js
@@ -0,0 +1,30 @@
+import { motion } from "framer-motion"
+
+export default function Button({ title, bgColor='#42ec9a', bgColorHover='#0fcc7d' }) {
+
+    const buttonVariants = {
+        hover: {
+            backgroundColor: bgColorHover,
+            transition: {
+                duration: 0.1
+            }
+        }
+    }
+
+    return (
+        <motion.a
+            variants={buttonVariants}
+            whileHover='hover'
+            href="#contact"
+            style={{
+                backgroundColor: bgColor,
+                ":hover": {
+                    backgroundColor: bgColorHover
+                }
+            }}
+            className={`text-[#2d3748] scale-100 p-4 px-12 sm:px-18 hover:shadow-2xl font-bold rounded-full text-base sm:text-xl`}
+        >
+            {title}
+        </motion.a>
+    )
+}
