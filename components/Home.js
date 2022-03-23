f26c6a407a155236925a50623350ab4aba54e650
Yuri Corredor <YuriCorredor159@gmail.com>
2022-03-23 15:19:50 -0300

    button is now a component

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { motion, useAnimation } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
+import Button from './Button'
 
 const outerDivVariants = {
     hidden: {
@@ -58,7 +59,9 @@ export default function Home() {
                 <p className="pb-8 text-[#0fcc7d] font-thin text-xs">ESCOLAS E CRECHES</p>
                 <h1 className="text-[#2d3748] text-5xl font-semibold p-4 pt-0 text-center sm:text-left">A parceira de mais de 50 Escolas e Creches brasileiras</h1>
                 <p className="text-[#718096] text-lg font-medium p-4 pb-0 pt-0 text-center sm:text-left">Trabalhamos com todos os equipamentos necessários para a sua Escola ou Creche. Entre em contato e faça o seu orçamento, e receberá ofertas incríveis.</p>
-                <a href="#contact" className="text-[#2d3748] p-4 px-12 sm:px-18 m-2 mt-12 md:mt-24 bg-[#42ec9a] hover:bg-[#0fcc7d] font-bold rounded-full text-base sm:text-xl">Contate-nos</a>
+                <div className='lg:mt-24 mt-12 mb-8 md:mb-0'>
+                    <Button title='Contate-nos' />
+                </div>
             </div>
             <motion.img
                 ref={ref}
