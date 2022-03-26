5aed89314a668ba04f6937fb02568f9dc7c77187
Yuri Corredor <YuriCorredor159@gmail.com>
2022-03-26 20:06:59 -0300

    Changed address

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ export default function Fotter() {
         <div className="bg-[#0fcc7d] mt-16 flex justify-center items-center">
             <div className="max-w-3xl text-center p-4 text-xs font-semibold text-[#2d3748]">
                 <p>Copyright © 2021 Fablec Comercio Varejista LTDA - CNPJ: 14.940.067/0001-10</p>
-                <p>Endereço: Rio de Janeiro / RJ, Rua Monsueto Menezes, S/N (Lot 23 Ltm 39668 Qdr 24) - Telefones: (21) 2415-8376 e (21) 99034-4765 </p>
+                <p>Endereço: Rio de Janeiro / RJ, Estrada da Posse, 998 - Telefones: (21) 2415-8376 e (21) 99034-4765 </p>
                 <h1 className="m-4 mb-0 font-extrabold text-3xl">FABLEC</h1>
             </div>
         </div>
